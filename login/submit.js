49f920bb3e485db7454087fb9579cbc08ea7e6aa
hikikomoriphoenix <dondon1987@yandex.com>
2018-07-26 19:21:36 +0800

    Add username and password validation

--- login/submit.js
+++ login/submit.js
@@ -1,1 +1,24 @@
-BACK_END_DIR = "https://hikikomoriphoenix.000webhostapp.com/Their-Legends/";
+/**
+ * Checks if username and password inputs are even valid before submitting
+ */
+function submitValidLogin() {
+    var username = document.getElementById("username");
+    var password = document.getElementById("password");    
+
+    if (validateLogin(username, password)) {
+        alert("Invalid username or password");
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function validateLogin(username, password) {
+    // Validate username
+    var usernameIsValid = /^[-\w\.\$@\*\!]{1,16}$/.test(username) ? true : false;
+
+    // Validate password
+    var passwordIsValid = /^[\S]{6,16}$/.test(password) ? true : false;
+
+    return usernameIsValid && passwordIsValid ? true : false;
+}
