5ebe2a9f309698452f86684b62e83253220bb7c5
hikikomoriphoenix <dondon1987@yandex.com>
2018-07-28 11:08:18 +0800

    Try CORS

--- consts.js
+++ consts.js
@@ -1,2 +1,3 @@
 // This file contains global constants
-BACK_END_DIR = "https://hikikomoriphoenix.000webhostapp.com/Their-Legends/";
+//BACK_END_DIR = "https://hikikomoriphoenix.000webhostapp.com/Their-Legends/";
+BACK_END_DIR = "http://hikikomoriphoenix.epizy.com/index.php";
